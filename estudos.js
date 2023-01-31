494501f09ce4a4ac89e7bd4cae39fae3ebd4581f
DjairAlmeida90 <djaptbr10@gmail.com>
2023-01-31 19:39:09 +0000

    estudosJS

---
+++ estudos.js
@@ -0,0 +1,43 @@
+//1-declarando a variável
+//let weight;
+
+//2-tipo de dado da variável 
+//console.log(typeof weight);
+//undefined
+
+//3-crie um objeto de  nome student com as propriedades name, age, weight e dê as propriedades os valores que desejar...;
+let student1 = {
+    name: "murilo",
+    age: 13,
+    weight: 53.7,
+};
+
+//4- crie um outro objeto e o coloque na posição 1 do Array abaixo:
+let student2 = {
+    name: "caua",
+    age: 17,
+    weight: 69.7,
+};
+delete student1.age
+
+//console.log(`${student.name} de idade ${student.age} pesa ${student.weight}kg.`);
+
+let students = [
+    student1,
+    student2
+];
+
+console.log(student1);
+
+
+
+
+
+
+
+
+
+
+
+
+
