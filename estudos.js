7c03bd64f552b792537f6c1fe23a7f13d85e1914
DjairAlmeida90 <djaptbr10@gmail.com>
2023-02-01 14:27:44 +0000

    continuando os estudos.JS

--- estudos.js
+++ estudos.js
@@ -6,7 +6,7 @@
 //undefined
 
 //3-crie um objeto de  nome student com as propriedades name, age, weight e dê as propriedades os valores que desejar...;
-let student1 = {
+/*let student1 = {
     name: "murilo",
     age: 13,
     weight: 53.7,
@@ -27,7 +27,35 @@ let students = [
     student2
 ];
 
-console.log(student1);
+console.log(student1);*/
+
+//Revisando functions.
+
+//function statement/declarção da função
+function appOfPrases() {
+    console.log('estudy is so good')
+    console.log('estudy is so great')
+    console.log('estudy is so welsome')
+}
+//call the function/ chamar a função
+appOfPrases()
+appOfPrases()
+appOfPrases()
+appOfPrases()
+
+//function  expression
+//number1 and number2 are the parameters
+//593 and 339 are the arguments
+//agruping code
+const sum = function(number1, number2) {
+    console.log(number1 + number2);
+}
+
+sum(593, 339);
+sum(593, 339);
+//reutilizando code
+sum(593, 339);
+
 
 
 
